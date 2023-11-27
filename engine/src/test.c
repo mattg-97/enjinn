dd1dc791672524e716e0799c3473cf42686c6ece
mattg-97 <mattjohngill@gmail.com>
2023-11-27 09:48:29 +0000

    added logging capabilities

--- engine/src/test.c
+++
@@ -1,6 +0,0 @@
-#include "test.h"
-#include <stdio.h>
-
-void print_int(i32 i) {
-    printf("Hello from kohi engine\n");
-}
--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,12 @@
-#include <test.h>
+#include <core/logger.h>
+#include <core/asserts.h>
 
 int main(void) {
-    print_int(42);
+    KFATAL("A fatal test message: %f", 3.14f);
+    KERROR("An error test message: %f", 3.14f);
+    KWARN("A warn test message: %f", 3.14f);
+    KINFO("An info test message: %f", 3.14f);
+    KDEBUG("A debug test message: %f", 3.14f);
+    KTRACE("A trace test message: %f", 3.14f);
     return 0;
 }
