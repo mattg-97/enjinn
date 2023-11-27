b58255c807627b32b4ff234fd063fda7506828d2
mattg-97 <mattjohngill@gmail.com>
2023-11-27 11:39:31 +0000

    added windows platform support

---
+++ engine/src/platform/platform_win32.c
@@ -0,0 +1,262 @@
+#include "platform/platform.h"
+
+// Windows platform layer
+#if KPLATFORM_WINDOWS
+
+#include <core/logger.h>
+#include <windows.h>
+#include <windowsx.h>
+#include <stdlib.h>
+
+typedef struct internal_state {
+    HINSTANCE h_instance;
+    HWND hwnd;
+} internal_state;
+
+// Clock
+static f64 clock_frequency;
+static LARGE_INTEGER start_time;
+
+// windows specific mumbo jumbo
+// take in handle to window, message being processed and some params
+LRESULT CALLBACK win32_process_message(HWND hwnd, u32 msg, WPARAM w_param, LPARAM l_param);
+
+b8 platform_startup(
+    platform_state* plat_state,
+    const char* application_name,
+    i32 x,
+    i32 y,
+    i32 width,
+    i32 height) {
+    plat_state->internal_state = malloc(sizeof(internal_state));
+    internal_state* state = (internal_state*)plat_state->internal_state;
+
+    // this reads 'give me a handle to the application that is currently executing
+    state->h_instance = GetModuleHandleA(0);
+
+    // setup and register window
+    // lets us display an icon
+    HICON icon = LoadIcon(state->h_instance, IDI_APPLICATION);
+    WNDCLASSA wc;
+    // zero out the memory
+    memset(&wc, 0, sizeof(wc));
+    // this style allows you to capture double clicks
+    wc.style = CS_DBLCLKS;
+    // this is a pointer to the window procedure, this handles events for us
+    wc.lpfnWndProc = win32_process_message;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    // pointing to our h instance
+    wc.hInstance = state->h_instance;
+    wc.hIcon = icon;
+    // tells the OS we will be managing the cursor on our own
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = NULL;
+    // the class name for the window
+    wc.lpszClassName = "enjinn_window_class";
+
+    if (!RegisterClassA(&wc)) {
+        // windows specific pop up for saying the window failed to register
+        MessageBoxA(0, "Window registration failed", "Error", MB_ICONEXCLAMATION | MB_OK);
+        return FALSE;
+    }
+
+    // Create window
+    u32 client_x = x;
+    u32 client_y = y;
+    u32 client_width = width;
+    u32 client_height = height;
+
+    u32 window_x = client_x;
+    u32 window_y = client_y;
+    u32 window_width = client_width;
+    u32 window_height = client_height;
+
+    u32 window_style = WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION;
+    u32 window_ex_style = WS_EX_APPWINDOW;
+
+    window_style |= WS_MAXIMIZEBOX;
+    window_style |= WS_MINIMIZEBOX;
+    window_style |= WS_THICKFRAME;
+
+    // Obtain the size of the border
+    RECT border_rect = {0, 0, 0, 0};
+    AdjustWindowRectEx(&border_rect, window_style, 0, window_ex_style);
+
+    // in this case, the border rectangle is negative
+    window_x += border_rect.left;
+    window_y += border_rect.top;
+
+    // Grow by the size of the OS border
+    window_width += border_rect.right - border_rect.left;
+    window_height += border_rect.bottom - border_rect.top;
+
+    HWND handle = CreateWindowExA(
+        window_ex_style, "enjinn_window_class", application_name, window_style, window_x, window_y,
+        window_width, window_height, 0, 0, state->h_instance, 0);
+
+    if (handle == 0) {
+        MessageBoxA(NULL, "Window creation failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+
+        KFATAL("Window creation failed");
+        return FALSE;
+    } else {
+        state->hwnd = handle;
+    }
+
+    // Show the window
+    b32 should_activate = 1;  // TODO: if the window should not accept input, this should be false
+    i32 show_window_command_flags = should_activate ? SW_SHOW : SW_SHOWNOACTIVATE;
+    // if initially minimized, use SW_MINIMIZE : SW_SHOWMINNOACTIVE
+    // if initially maximized, use SW_SHOWMAXIMIZED : SW_MAXIMIZE
+    ShowWindow(state->hwnd, show_window_command_flags);
+
+    // Clock setup
+    LARGE_INTEGER frequency;
+    QueryPerformanceFrequency(&frequency);
+    clock_frequency = 1.0 / (f64)frequency.QuadPart;
+    QueryPerformanceCounter(&start_time);
+
+    return TRUE;
+}
+
+void platform_shutdown(platform_state* plat_state) {
+    // Simply cold cast to the known type
+    internal_state* state = (internal_state*)plat_state->internal_state;
+
+    if (state->hwnd) {
+        DestroyWindow(state->hwnd);
+        state->hwnd = 0;
+    }
+}
+
+b8 platform_pump_messages(platform_state* plat_state) {
+    // MSG is just a simple structure/opaque type
+    MSG message;
+    // PeekMessageA basically says 'if there is a message on the stack to be processed, fill MSG with it, then remove it'
+    // if we dont remove it, it will become unresponsive
+    while (PeekMessageA(&message, NULL, 0, 0, PM_REMOVE)) {
+        TranslateMessage(&message);
+        // dispatch fires off a message according to whatever we have defined in our win32_process_message
+        DispatchMessageA(&message);
+    }
+
+    return TRUE;
+}
+
+void* platform_allocate(u64 size, b8 aligned) {
+    return malloc(size);
+}
+
+void platform_free(void* block, b8 aligned) {
+    free(block);
+}
+
+void* platform_zero_memory(void* block, u64 size) {
+    return memset(block, 0, size);
+}
+
+void* platform_copy_memory(void* dest, const void* source, u64 size) {
+    return memcpy(dest, source, size);
+}
+
+void* platform_set_memory(void* dest, i32 value, u64 size) {
+    return memset(dest, value, size);
+}
+
+void platform_console_write(const char* message, u8 colour) {
+    HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    // FATAL, ERROR, WARN, INFO, DEBUG, TRACE
+    static u8 levels[6] = {64, 4, 6, 2, 1, 8};
+    SetConsoleTextAttribute(console_handle, levels[colour]);
+
+    // seperate windows specific output stream
+    OutputDebugStringA(message);
+    u64 length = strlen(message);
+    LPDWORD number_written = 0;
+    WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), message, (DWORD)length, number_written, 0);
+}
+
+void platform_console_write_error(const char* message, u8 colour) {
+    HANDLE console_handle = GetStdHandle(STD_ERROR_HANDLE);
+    // FATAL, ERROR, WARN, INFO, DEBUG, TRACE
+    static u8 levels[6] = {64, 4, 6, 2, 1, 8};
+    SetConsoleTextAttribute(console_handle, levels[colour]);
+
+    // seperate windows specific output stream
+    OutputDebugStringA(message);
+    u64 length = strlen(message);
+    LPDWORD number_written = 0;
+    WriteConsoleA(GetStdHandle(STD_ERROR_HANDLE), message, (DWORD)length, number_written, 0);
+}
+
+f64 platform_get_absolute_time() {
+    LARGE_INTEGER now_time;
+    QueryPerformanceCounter(&now_time);
+    return (f64)now_time.QuadPart * clock_frequency;
+}
+
+void platform_sleep(u64 ms) {
+    Sleep(ms);
+}
+
+LRESULT CALLBACK win32_process_message(HWND hwnd, u32 msg, WPARAM w_param, LPARAM l_param) {
+    switch (msg) {
+        case WM_ERASEBKGND:
+            // Notify the OS that erasing the screen is handled by the engine, and should stop flickering
+            // return 1 is explicity set my microsofts docs
+            return 1;
+        case WM_CLOSE:
+            // TODO: fire an event for the application to quit
+            return 0;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        case WM_SIZE: {
+            // Get the updated size of the window
+            // RECT r;
+            // GetClientRect(hwnd, &r);
+            // u32 width = r.right - r.left;
+            // u32 height = r.bottom - r.top;
+
+            // TODO: fire an event for window resizing
+        } break;
+        case WM_KEYDOWN:
+        case WM_SYSKEYDOWN:
+        case WM_KEYUP:
+        case WM_SYSKEYUP: {
+            // key pressed/released
+            // b8 pressed = (msg == WM_KEYDOWN || msg == WM_SYSKEYDOWN);
+            //  TODO: input processing
+        } break;
+        case WM_MOUSEMOVE: {
+            // Mouse move
+            // i32 x_position = GET_X_LPARAM(l_param);
+            // i32 y_position = GET_Y_LPARAM(l_param);
+
+            // TODO: input processing
+        } break;
+        case WM_MOUSEWHEEL: {
+            // wheel delta is the mouse wheel going up and down
+            i32 z_delta = GET_WHEEL_DELTA_WPARAM(w_param);
+            if (z_delta != 0) {
+                // Flatten the input to an OS-Independant (-1, 1)
+                // only fires if the z_delta isnt 0, which means that something has happened with the mouse wheel
+                // z_delta = (z_delta < 0) ? -1 : 1;
+                // TODO: input processing
+            }
+        } break;
+        case WM_LBUTTONDOWN:
+        case WM_MBUTTONDOWN:
+        case WM_RBUTTONDOWN:
+        case WM_LBUTTONUP:
+        case WM_MBUTTONUP:
+        case WM_RBUTTONUP: {
+            // b8 pressed = msg == WM_LBUTTONDOWN || msg == WM_RBUTTONDOWN || msg == WM_MBUTTONDOWN;
+            //  TODO input processing
+        } break;
+    }
+    return DefWindowProcA(hwnd, msg, w_param, l_param);
+}
+
+#endif  // KPLATFORM_WINDOWS
