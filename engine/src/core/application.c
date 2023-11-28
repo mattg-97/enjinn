df92a81825a02a4f3488060ee0798146014e7a5b
mattg-97 <mattjohngill@gmail.com>
2023-11-28 13:57:08 +0000

    added events subsystem

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -3,6 +3,7 @@
 #include "platform/platform.h"
 #include "game_types.h"
 #include "core/kmemory.h"
+#include "core/event.h"
 
 typedef struct application_state {
     game* game_inst;
@@ -38,6 +39,11 @@ b8 application_create(game* game_inst) {
 
     app_state.is_running = TRUE;
     app_state.is_suspended = FALSE;
+
+    if (!event_initialize()) {
+        KERROR("Event sustem failed initialization, cannot continue");
+        return FALSE;
+    }
 
     if (!platform_startup(&app_state.platform,
                           game_inst->app_config.name,
@@ -86,6 +92,8 @@ b8 application_run() {
     }
 
     app_state.is_running = FALSE;
+
+    event_shutdown();
 
     platform_shutdown(&app_state.platform);
 
